33fdabe687b9bd430cbddcfe8828801ba8287250
hyl <3307378337@qq.com>
2024-03-26 09:30:14 +0800

    解决vp样式冲突

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { postcssIsolateStyles } from 'vitepress'
 
 export default {
-  plugins: [tailwindcss, autoprefixer]
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    postcssIsolateStyles({
+      includeFiles: [/vp-doc\.css/] // defaults to /base\.css/
+    })
+  ]
 }
