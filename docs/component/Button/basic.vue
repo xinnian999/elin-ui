ac048af734902e42dd62e0de320dd8fc32e9b946
hyl <3307378337@qq.com>
2024-04-30 17:45:16 +0800

    文档升级+

---
+++ docs/component/Button/basic.vue
@@ -0,0 +1,8 @@
+<template>
+  <e-button>Default</e-button>
+  <e-button type="primary">Primary</e-button>
+  <e-button type="success">Success</e-button>
+  <e-button type="info">Info</e-button>
+  <e-button type="warning">Warning</e-button>
+  <e-button type="danger">danger</e-button>
+</template>
