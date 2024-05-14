12e087d439a0b7646a5010edfff1888a8952847b
hyl <3307378337@qq.com>
2024-05-14 14:07:43 +0800

    文档+

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,22 +31,42 @@ export default defineConfig({
         {
           text: '基础组件',
           items: [
-            { text: 'Button 按钮', link: `/component/Button/` },
+            { text: 'Button 按钮', link: `/component/Button/` }
             // { text: 'Space 间距', link: '/component/Space' },
-            { text: 'Dialog 对话框', link: '/component/Dialog/' },
-            { text: 'Table 表格', link: '/component/Table/' },
+          ]
+        },
+        {
+          text: '表单组件',
+          items: [
             { text: 'Input 输入框', link: '/component/Input/' },
             { text: 'Checkbox 多选框', link: '/component/Checkbox/' },
-            { text: 'Tooltip 文字提示', link: '/component/Tooltip/' },
-            { text: 'Popover 悬浮气泡', link: '/component/Popover/' },
-            { text: 'Dropdown 下拉菜单', link: '/component/Dropdown/' },
-            { text: 'Select 选择框', link: '/component/Select/' },
-            { text: 'Menu 菜单', link: '/component/Menu/' },
+            { text: 'Select 选择框', link: '/component/Select/' }
+          ]
+        },
+        {
+          text: '数据展示',
+          items: [
+            { text: 'Table 表格', link: '/component/Table/' },
             { text: 'List 列表', link: '/component/List/' }
+          ]
+        },
+        {
+          text: '反馈组件',
+          items: [
+            { text: 'Dialog 对话框', link: '/component/Dialog/' },
+            { text: 'Tooltip 文字提示', link: '/component/Tooltip/' },
+            { text: 'Popover 悬浮气泡', link: '/component/Popover/' }
+          ]
+        },
+        {
+          text: '导航组件',
+          items: [
+            { text: 'Dropdown 下拉菜单', link: '/component/Dropdown/' },
+            { text: 'Menu 菜单', link: '/component/Menu/' }
           ]
         }
       ]
     },
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+    socialLinks: [{ icon: 'github', link: 'https://github.com/xinnian999/elin-ui' }]
   }
 })
