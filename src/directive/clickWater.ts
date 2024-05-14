895ad6812a5b13872da05de9278269975dfc57b9
hyl <3307578337@qq.com>
2024-05-14 22:33:48 +0800

    ok

--- src/directive/clickWater.ts
+++ src/directive/clickWater.ts
@@ -2,18 +2,20 @@ import { type App, nextTick } from 'vue'
 import type { $Config } from '@/config'
 
 const clickWater = (app: App<Element>, config: $Config) => {
+  const handle = async (el: HTMLElement) => {
+    await nextTick()
+    const { ns } = config
+
+    el.classList.add(ns('click-water'))
+
+    el.addEventListener('mousedown', (e: MouseEvent) => {
+      const { left, top } = el.getBoundingClientRect()
+      el.style.cssText = `--x:${e.clientX - left}px;--y:${e.clientY - top}px`
+    })
+  }
   app.directive('click-water', {
-    updated: async (el: HTMLElement) => {
-      await nextTick()
-      const { ns } = config
-
-      el.classList.add(ns('click-water'))
-
-      el.addEventListener('mousedown', (e: MouseEvent) => {
-        const { left, top } = el.getBoundingClientRect()
-        el.style.cssText = `--x:${e.clientX - left}px;--y:${e.clientY - top}px`
-      })
-    }
+    mounted: handle,
+    updated: handle
   })
 }
 
